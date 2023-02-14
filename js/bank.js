316eab5629606a649481d7e3d3a0d6725403acd7
emran <shuvoorba28@gmail.com>
2023-02-14 10:38:28 +0600

    first commit

---
+++ js/bank.js
@@ -0,0 +1,7 @@
+document.getElementById('button-deposit').addEventListener('click', function(){
+    const depositInput = document.getElementById('input-deposit');
+    const depositTotal = depositInput.value;
+    const depositAmount = document.getElementById('deposit-total');
+    
+
+})
